97628948a397d8fb3c84eddb894009744d4339aa
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-07-25 19:57:01 +0900

    [Silver III] Title: N과 M (5), Time: 348 ms, Memory: 29024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static boolean c[];
+    static int a[];
+
+    static int b[];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        c = new boolean[n + 1];
+        a = new int[n + 1];
+        b =new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++)
+        {
+            b[i]= Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(b);
+
+        go(0);
+
+        System.out.println(sb);
+    }
+
+    public static void go(int index) {
+        if (index == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(a[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i <n; i++) {
+            if (c[i]) continue;
+            c[i] = true;
+            a[index] = b[i];
+            go(index + 1);
+            c[i] = false;
+        }
+    }
+}
+
