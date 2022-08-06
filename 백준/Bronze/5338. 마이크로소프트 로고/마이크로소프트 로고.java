9a9dda70e2c3af46cb1b672dfc1985375ee1f830
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-06 15:06:33 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 120 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._").append("\n").append("'-..-'|   ||   |").append("\n").append("'-..-'|_.-;;-._|").append("\n").append("'-..-'|   ||   |").append("\n").append("'-..-'|_.-''-._|");
+        System.out.println(sb);
+    }
+}
