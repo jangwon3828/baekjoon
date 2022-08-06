8b7cc2c3010f4f06cb87aa024edce6bcd9a98a1e
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-06 15:09:05 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+              sb.append("     /~\\").append("\n")
+                .append("    ( oo|").append("\n")
+                .append("    _\\=/_").append("\n")
+                .append("   /  _  \\").append("\n")
+                .append("  //|/.\\|\\\\").append("\n")
+                .append(" ||  \\ /  ||").append("\n")
+                .append("============").append("\n")
+                .append("|          |").append("\n")
+                .append("|          |").append("\n")
+                .append("|          |");
+
+        System.out.println(sb);
+    }
+}
