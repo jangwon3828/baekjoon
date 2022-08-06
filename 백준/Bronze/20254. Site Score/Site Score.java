26415e5ff6c07bcd9ceb0ace14dea1bfad93c4d1
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-06 17:57:18 +0900

    [Bronze V] Title: Site Score, Time: 228 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int sum=a*56+b*24+c*14+d*6;
+        System.out.println(sum);
+    }
+
+}
