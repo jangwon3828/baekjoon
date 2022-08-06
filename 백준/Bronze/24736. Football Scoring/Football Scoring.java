727076264a78a60752691fb8b4746dc583ca44cd
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-06 18:22:03 +0900

    [Bronze V] Title: Football Scoring, Time: 128 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        sb.append(a*6+b*3+c*2+d+e*2).append("\n");
+        st=new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        sb.append(a*6+b*3+c*2+d+e*2);
+        System.out.println(sb);
+    }
+}
