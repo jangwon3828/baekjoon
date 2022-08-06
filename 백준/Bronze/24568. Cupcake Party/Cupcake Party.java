a715514298993168dcf123458a45836a191d4f4f
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-06 18:19:19 +0900

    [Bronze V] Title: Cupcake Party, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int result=(a*8+b*3) -28;
+        System.out.println(result);
+    }
+}
