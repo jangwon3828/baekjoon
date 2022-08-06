e7f4370ae9010a8e807b29ed0f5d93cdf6c79761
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-06 16:59:49 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 156 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        System.out.println(String.valueOf((K / M)+" "+(K - (M * (K / M)))));
+    }
+}
