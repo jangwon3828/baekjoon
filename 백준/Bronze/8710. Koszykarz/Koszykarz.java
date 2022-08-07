50ecf0ac81d58e885c28f6d9260dd2c01b9a618a
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-07 23:44:46 +0900

    [Bronze IV] Title: Koszykarz, Time: 652 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/8710. Koszykarz/Koszykarz.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int count = 0;
+        for (int i = a; i < b; i += c) count++;
+        System.out.println(count);
+
+    }
+}
