11d3fb26945f72894b5048a1b1d898ea0d790d0c
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-07 23:16:49 +0900

    [Bronze IV] Title: Can you add this?, Time: 232 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println(sc.nextLong() + sc.nextLong());
+        }
+    }
+}
