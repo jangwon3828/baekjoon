50a8a5bdfec75e397a3349866287400000e79a81
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-08-18 12:14:28 +0900

    [Bronze IV] Title: 연세대학교, Time: 204 ms, Memory: 17508 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N;
+        N = sc.nextInt();
+        if( N == 1 )
+            System.out.println("Leading the Way to the Future");
+        else
+            System.out.println("YONSEI");
+    }
+}
