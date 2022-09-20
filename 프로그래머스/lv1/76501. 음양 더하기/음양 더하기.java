00b3f118b9f657ff579d7c5f5cc0d76ffccdf4e9
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-09-20 20:34:05 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum=0;
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i]){
+                sum+=absolutes[i];
+            }else{
+                 sum-=absolutes[i];
+            }
+        }
+        return sum;
+    }
+}
