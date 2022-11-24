0e8dfe140f137cf5b0183416cc61cf5a02f57906
jangwon3828 <105543967+jangwon3828@users.noreply.github.com>
2022-11-24 21:13:56 +0900

    [Bronze II] Title: 커트라인, Time: 268 ms, Memory: 20208 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int cut = sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[n-cut]);
+    }
+}
