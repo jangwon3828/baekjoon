d0907a49af8549dd50a4b3d1d81fe3e154dc54e6
장원진 <105543967+jangwon3828@users.noreply.github.com>
2022-11-28 21:18:22 +0900

    [unrated] Title: 귤 고르기, Time: 54.24 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -2,20 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-     int answer=0;
+      int answer=0;
         Map<Integer, Integer> map = new HashMap<>();
         for( int i : tangerine){
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        List<Integer> list = new ArrayList<>(map.keySet());
-        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
-
-        for(Integer x : list){
-            k -= map.get(x);
+        ArrayList<Integer> list = new ArrayList<>(map.values());
+        Collections.sort(list);
+        for (int i = list.size() - 1; i >=0 ; i--) {
             answer++;
-            if(k <= 0){
-                break;
-            }
+            k-=list.get(i);
+            if(k<=0)break;
         }
         return answer;
     }
