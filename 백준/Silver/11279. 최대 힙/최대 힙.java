98fefa6de05287436b179ecfdb1fecf70e4b723e
장원진 <105543967+jangwon3828@users.noreply.github.com>
2022-11-30 21:30:19 +0900

    [Silver II] Title: 최대 힙, Time: 1032 ms, Memory: 112884 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> tree = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        for (int i = 0; i < num; i++) {
+            int n = sc.nextInt();
+            if (n == 0 && tree.isEmpty()) {
+                sb.append(0).append("\n");
+            } else if (n == 0) {
+                Integer first = tree.poll();
+                sb.append(first).append("\n");
+            } else {
+                tree.add(n);
+            }
+        }
+        System.out.println(sb);
+    }
+}
