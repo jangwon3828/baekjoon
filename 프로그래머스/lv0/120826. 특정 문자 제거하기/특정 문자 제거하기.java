7654f3616e8ce21d3189553d951f184321a18af0
장원진 <105543967+jangwon3828@users.noreply.github.com>
2022-12-19 16:51:33 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        answer=my_string.replace(letter,"");
+        return answer;
+    }
+}
