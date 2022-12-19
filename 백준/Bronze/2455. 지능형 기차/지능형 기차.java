7483ff6c605b48b5815b1e01dc532f7b85d2dc91
장원진 <105543967+jangwon3828@users.noreply.github.com>
2022-12-19 10:03:59 +0900

    [Bronze III] Title: 지능형 기차, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        int max=0;
+        for (int i = 0; i < 4; i++) {
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            count=count+m-n;
+            max=Math.max(count,max);
+        }
+        System.out.println(max);
+    }
+}
