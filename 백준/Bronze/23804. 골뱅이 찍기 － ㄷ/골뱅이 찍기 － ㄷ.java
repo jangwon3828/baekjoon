d34c0bcb95acbc3fd8de985c86650ed66f5ff8c1
장원진 <105543967+jangwon3828@users.noreply.github.com>
2022-12-21 10:35:53 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 244 ms, Memory: 19684 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        String s="@";
+        String repeat = s.repeat(n);
+        String first=repeat;
+        String second=repeat;
+
+        for (int i = 0; i < 4; i++) {
+            first=first+repeat;
+        }
+        for (int i = 0; i < n-1; i++) {
+            second=second+"\n"+repeat;
+        }
+        StringBuilder sb=new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(first).append("\n");
+        }
+        for (int i = 0; i < 3; i++) {
+            sb.append(second).append("\n");
+        }
+        for (int i = 0; i < n; i++) {
+            sb.append(first).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
