82877528fd63481641d81d486de6500f18383de2
장원진 <105543967+jangwon3828@users.noreply.github.com>
2022-12-22 09:37:48 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 284 ms, Memory: 24884 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb= new StringBuilder();
+        int n = sc.nextInt();
+        String s = "@";
+        String empty = " ";
+        String repeat2 = s.repeat(n)+empty.repeat(n*3)+s.repeat(n);
+        String repeat = s.repeat(n*5);
+        String first = "";
+        String second = "";
+        String third = "";
+
+
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                first = first + repeat;
+            } else first = first + repeat + "\n";
+        }
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                second = second + repeat2;
+            } else
+                second = second + repeat2 + "\n";
+        }
+        sb.append(second).append("\n");
+        sb.append(first).append("\n");
+        sb.append(second).append("\n");
+        sb.append(first).append("\n");
+        sb.append(second).append("\n");
+
+
+        System.out.println(sb);
+    }
+}
