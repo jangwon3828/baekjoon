2103f518c18bdcbb3623f347872784c8c727286c
장원진 <wkd3828@gmail.com>
2022-12-23 15:55:15 +0900

    [Silver II] Title: N과 M (12), Time: 704 ms, Memory: 34564 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class Main {
+
+    static int[] map;
+    static int[] arr;
+    static int n;
+    static int m;
+    static LinkedHashSet<String> strings;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        map = new int[n];
+        arr = new int[m];
+        strings = new LinkedHashSet<>();
+
+
+
+        for (int i = 0; i < n; i++) {
+            map[i] = sc.nextInt();
+        }
+        Arrays.sort(map);
+        go(0);
+        strings.forEach(System.out::println);
+    }
+
+    private static void go(int count) {
+        if(count==m){
+            for (int i = 0; i < m-1; i++) {
+                if(arr[i]<=arr[i+1])continue;
+                return;
+            }
+            StringBuilder s= new StringBuilder();
+            for(int a:arr){
+                s.append(a).append(" ");
+            }
+            strings.add(s.toString());
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[count]=map[i];
+            go(count+1);
+        }
+    }
+}
