002a59d2578e036df8620b06d8986741fa17a734
장원진 <wkd3828@gmail.com>
2022-12-23 14:53:46 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 212 ms, Memory: 18372 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,54 +1,50 @@
 import java.util.*;
-class Pair {
-    public int min, max;
-    Pair(int min, int max) {
-        this.min = min;
-        this.max = max;
-    }
-}
+
 public class Main {
-    static Pair calc(int[] a, int index, int cur, int plus, int minus, int mul, int div) {
-        int n = a.length;
-        if (index == n) {
-            return new Pair(cur, cur);
-        }
-        ArrayList<Pair> res = new ArrayList<>();
-        if (plus > 0){
-            res.add(calc(a,index+1,cur+a[index],plus-1,minus,mul,div));
-        }
-        if (minus > 0) {
-            res.add(calc(a,index+1,cur-a[index],plus,minus-1,mul,div));
-        }
-        if (mul > 0) {
-            res.add(calc(a,index+1,cur*a[index],plus,minus,mul-1,div));
-        }
-        if (div > 0) {
-            res.add(calc(a,index+1,cur/a[index],plus,minus,mul,div-1));
-        }
-        Pair ans = res.get(0);
-        for (Pair p : res) {
-            if (ans.max < p.max) {
-                ans.max = p.max;
-            }
-            if (ans.min > p.min) {
-                ans.min = p.min;
-            }
-        }
-        return ans;
-    }
+
+    static int max=Integer.MIN_VALUE;
+    static int min=Integer.MAX_VALUE;
+    static int[] operator=new int[4];
+    static int[] arr;
+    static int n;
+
     public static void main(String[] args) {
+        init();
+        go(arr[0],0);
+        System.out.println(max);
+        System.out.println(min);
+    }
+    public static void init(){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i=0; i<n; i++) {
-            a[i] = sc.nextInt();
+        n = sc.nextInt();
+        arr=new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
         }
-        int plus = sc.nextInt();
-        int minus = sc.nextInt();
-        int mul = sc.nextInt();
-        int div = sc.nextInt();
-        Pair ans = calc(a, 1, a[0], plus, minus, mul, div);
-        System.out.println(ans.max);
-        System.out.println(ans.min);
+        for (int i = 0; i < 4; i++) {
+            operator[i]= sc.nextInt();
+        }
+        sc.close();
+
+    }
+    public static void go(int num,int index) {
+        if(index==n-1){
+            max=Math.max(max,num);
+            min=Math.min(min,num);
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            if(operator[i]>0){
+                operator[i]--;
+                switch (i){
+                    case 0: go(num+arr[index+1],index+1); break;
+                    case 1: go(num-arr[index+1],index+1); break;
+                    case 2: go(num*arr[index+1],index+1); break;
+                    case 3: go(num/arr[index+1],index+1); break;
+                }
+                operator[i]++;
+            }
+
+        }
     }
 }
