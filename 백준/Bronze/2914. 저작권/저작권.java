00fcdd2d1f7204759947037dce06b83dc1fceb10
장원진 <wkd3828@gmail.com>
2022-12-25 20:57:05 +0900

    [Bronze III] Title: 저작권, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        System.out.println(String.valueOf(A*(I-1)+1));
+
+    }
+}
