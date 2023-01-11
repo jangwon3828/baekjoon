8fe6d570ad311e03bc8a4581f147ef7771b2250b
장원진 <wkd3828@gmail.com>
2023-01-11 10:55:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 340 ms, Memory: 22552 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,41 +1,44 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-    public static final int MAX = 1000000;
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        boolean[] check = new boolean[MAX];
-        int[] dist = new int[MAX];
-        check[n] = true;
-        dist[n] = 0;
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(n);
-        while (!q.isEmpty()) {
-            int now = q.remove();
-            if (now-1 >= 0) {
-                if (check[now-1] == false) {
-                    q.add(now-1);
-                    check[now-1] = true;
-                    dist[now-1] = dist[now] + 1;
+        int min = Integer.MAX_VALUE;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{n, 0});
+        boolean[] check = new boolean[100001];
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int num = poll[0];
+            int cnt = poll[1];
+            if(num==m){
+                min=Math.min(min,cnt);
+                continue;
+            }
+            if (num - 1 >= 0) {
+                if (!check[num - 1]) {
+                    check[num - 1] = true;
+                    queue.offer(new int[]{num - 1, cnt+1});
                 }
             }
-            if (now+1 < MAX) {
-                if (check[now+1] == false) {
-                    q.add(now+1);
-                    check[now+1] = true;
-                    dist[now+1] = dist[now] + 1;
+            if (num + 1 <= 100000) {
+                if (!check[num + 1]) {
+                    check[num + 1] = true;
+                    queue.offer(new int[]{num + 1, cnt+1});
                 }
             }
-            if (now*2 < MAX) {
-                if (check[now*2] == false) {
-                    q.add(now*2);
-                    check[now*2] = true;
-                    dist[now*2] = dist[now] + 1;
+            if (num * 2 <= 100000) {
+                if (!check[num * 2]) {
+                    check[num * 2] = true;
+                    queue.offer(new int[]{num * 2, cnt+1});
                 }
             }
+
         }
-        System.out.println(dist[m]);
+        System.out.println(min);
     }
 }
