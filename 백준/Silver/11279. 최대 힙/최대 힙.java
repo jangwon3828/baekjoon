0cf9b2fe958167ec45707c2afbc814b93c7f439c
장원진 <wkd3828@gmail.com>
2023-01-12 10:22:55 +0900

    [Silver II] Title: 최대 힙, Time: 1024 ms, Memory: 114344 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,25 +1,21 @@
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
-        PriorityQueue<Integer> tree = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2-o1;
-            }
-        });
-        for (int i = 0; i < num; i++) {
-            int n = sc.nextInt();
-            if (n == 0 && tree.isEmpty()) {
-                sb.append(0).append("\n");
-            } else if (n == 0) {
-                Integer first = tree.poll();
-                sb.append(first).append("\n");
-            } else {
-                tree.add(n);
+
+        int n = sc.nextInt();
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Comparator.reverseOrder());
+        StringBuilder sb=new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int m = sc.nextInt();
+            if(m==0){
+                sb.append(pq.size()==0?0:pq.poll()).append("\n");
+
+            }else {
+                pq.offer(m);
             }
         }
         System.out.println(sb);
