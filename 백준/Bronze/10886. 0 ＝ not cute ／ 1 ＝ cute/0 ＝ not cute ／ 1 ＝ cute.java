d46cbf12e99e7bdca5687f6cf6e5c088e662bf3f
장원진 <wkd3828@gmail.com>
2023-01-21 14:32:33 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.java
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.java
@@ -1,13 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int t = sc.nextInt();
-        int cute = 0;
-        for (int i = 0; i < t; i++) {
-            cute+= sc.nextInt();
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c=0;
+        for(int i=0; i<a;i++){
+            int b = sc.nextInt();
+            if(b==0){
+                c++;
+            }
+            else if(b==1){
+                c--;
+            }
         }
-        System.out.println(cute>t/2?"Junhee is cute!":"Junhee is not cute!");
+        if(c>0){
+            System.out.println("Junhee is not cute!");
+        }
+        else if(c<0){
+            System.out.println("Junhee is cute!");
+        }
     }
 }
