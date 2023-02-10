f665482c7f9a3fd43bf8d1fd2eb68c2d0d3d5379
장원진 <wkd3828@gmail.com>
2023-02-10 11:24:55 +0900

    [level 2] Title: 예상 대진표, Time: 0.21 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer=0;
+        while (true){
+           a=a/2+a%2;
+        b=b/2+b%2;
+            answer++;
+        if(a==b){
+            break;
+        }
+    }
+
+        System.out.println(answer);
+        return answer;
+    }
+}
