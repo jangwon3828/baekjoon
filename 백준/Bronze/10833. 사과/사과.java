2037afb35b81001e5fb0af8cff01d7acac5185aa
장원진 <wkd3828@gmail.com>
2023-02-17 09:29:53 +0900

    [Bronze III] Title: 사과, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int sum = 0;
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			sum += (B % A);
+		}
+		System.out.println(sum);
+	}
+
+}
