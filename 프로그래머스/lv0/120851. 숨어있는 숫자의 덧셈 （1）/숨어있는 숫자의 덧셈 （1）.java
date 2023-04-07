6a20179681c3fd2398f9eb7246016d6faca7581f
장원진 <wkd3828@gmail.com>
2023-04-07 18:33:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.27 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string) {
+      int answer = 0;
+        
+        String replace = "";
+		replace = my_string.toLowerCase().replaceAll("[abcdefghijklmnopqrstuvwxyz]", "");
+		String[] arry = replace.split("");
+
+            for(int i = 0; i< arry.length; i++) {
+                answer += Integer.parseInt(arry[i]);
+            }
+
+        
+        return answer;
+    }
+}
