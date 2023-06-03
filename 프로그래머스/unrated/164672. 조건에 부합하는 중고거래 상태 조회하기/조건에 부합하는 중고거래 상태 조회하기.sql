377859d699fefa32969897153619672d6f658dac
장원진 <wkd3828@gmail.com>
2023-06-04 00:03:28 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+  CASE
+    WHEN STATUS = 'SALE' THEN '판매중'
+    WHEN STATUS = 'RESERVED' THEN '예약중'
+    WHEN STATUS = 'DONE' THEN '거래완료'
+    ELSE '알 수 없음'
+  END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE created_date = '2022-10-05'
+order by board_id desc
