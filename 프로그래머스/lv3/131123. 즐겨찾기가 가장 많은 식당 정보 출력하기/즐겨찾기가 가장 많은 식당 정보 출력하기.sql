d60e42d69905f3c8c41eb71a8bb80ae4862cd3d1
장원진 <wkd3828@gmail.com>
2023-07-27 11:01:10 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,9 @@
+SELECT food_type, rest_id, rest_name, favorites
+FROM rest_info
+WHERE (food_type, favorites) IN (
+    SELECT food_type, MAX(favorites)
+    FROM rest_info
+    GROUP BY food_type
+)
+group by food_type
+ORDER BY food_type DESC;
